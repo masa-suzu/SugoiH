0b2935125f9bff22eb9c50551cafd0d57e05acd1
masa-suzu <11026517+masa-suzu@users.noreply.github.com>
2018-12-31 20:52:05 +0900

    ch2p26:一般的なHaskellの型

---
+++ src/ch2/GeneralTypes.hs
@@ -0,0 +1,8 @@
+factrorial :: Integer -> Integer
+factrorial n = product [1 .. n]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
