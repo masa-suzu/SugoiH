6b28d4993639a4b662dfad9d936d6609d1d0cadd
masa-suzu <11026517+masa-suzu@users.noreply.github.com>
2019-01-02 01:39:36 +0900

    ch5p75:ラムダ式

--- src/ch5/Examples.hs
+++ src/ch5/Examples.hs
@@ -4,11 +4,15 @@ zipWith' _ _ [] = []
 zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 
 flip' :: (a -> b -> c) -> (b -> a -> c)
-flip' f = g 
-    where g x y = f y x
+flip' f = g
+  where
+    g x y = f y x
 
 flip'' :: (a -> b -> c) -> b -> a -> c
 flip'' f x y = f y x
+
+flip''' :: (a -> b -> c) -> b -> a -> c
+flip''' f = \x y -> f y x
 
 map' :: (a -> b) -> [a] -> [b]
 map' _ [] = []
@@ -17,19 +21,25 @@ map' f (x:xs) = f x : map' f xs
 filter' :: (a -> Bool) -> [a] -> [a]
 filter' _ [] = []
 filter' f (x:xs)
-    | f x == True = x : filter' f xs
-    | otherwise = filter' f xs
+  | f x == True = x : filter' f xs
+  | otherwise = filter' f xs
 
 largestDivisible :: Integer
-largestDivisible = head (filter' p [10000,9999..])
-    where p x = x `mod` 3829 == 0
+largestDivisible = head (filter' p [10000,9999 ..])
+  where
+    p x = x `mod` 3829 == 0
 
 chain :: Integer -> [Integer]
 chain 1 = [1]
 chain n
-    | even n = n : chain (n `div` 2)
-    | odd n = n : chain (n * 3 +1)
+  | even n = n : chain (n `div` 2)
+  | odd n = n : chain (n * 3 + 1)
 
 numLongChains :: Int
-numLongChains = length (filter' isLong (map' chain [1..100]))
-    where isLong xs = length xs > 15
+numLongChains = length (filter' isLong (map' chain [1 .. 100]))
+  where
+    isLong xs = length xs > 15
+
+numLongChains' :: Int
+numLongChains' =
+  length (filter' (\xs -> length xs > 15) (map' chain [1 .. 100]))
