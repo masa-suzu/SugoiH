fcf857f29902a570e909f392f02a421675fd17b8
masa-suzu <11026517+masa-suzu@users.noreply.github.com>
2019-01-02 01:01:48 +0900

    ch4p59:クイック、ソート！

--- src/ch4/Recursions.hs
+++ src/ch4/Recursions.hs
@@ -1,13 +1,13 @@
 replicate' :: Int -> a -> [a]
 replicate' n x
-    | n <= 0 = []
-    | otherwise = x : replicate' (n-1) x
+  | n <= 0 = []
+  | otherwise = x : replicate' (n - 1) x
 
 take' :: Int -> [a] -> [a]
 take' n _
-    | n <= 0 = []
+  | n <= 0 = []
 take' _ [] = []
-take' n (x:xs) = x : take' (n-1) xs
+take' n (x:xs) = x : take' (n - 1) xs
 
 reverse' :: [a] -> [a]
 reverse' [] = []
@@ -16,13 +16,13 @@ reverse' (x:xs) = reverse' xs ++ [x]
 repeat' :: a -> [a]
 repeat' x = x : repeat' x
 
-zip' :: [a] -> [b] -> [(a,b)]
+zip' :: [a] -> [b] -> [(a, b)]
 zip' _ [] = []
 zip' [] _ = []
-zip' (x:xs) (y:ys) = (x,y) : zip' xs ys
+zip' (x:xs) (y:ys) = (x, y) : zip' xs ys
 
-elem' :: (Eq a) => a -> [a]-> Bool
+elem' :: (Eq a) => a -> [a] -> Bool
 elem' _ [] = False
 elem' a (x:xs)
-    | a == x = True
-    | otherwise = elem' a xs
+  | a == x = True
+  | otherwise = elem' a xs
