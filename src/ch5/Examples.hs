427d37815ecbe6ea3d0cf8e4aeb4a1137fcbd44b
masa-suzu <11026517+masa-suzu@users.noreply.github.com>
2019-01-02 01:34:53 +0900

    ch5p72:関数プログラマの道具箱

--- src/ch5/Examples.hs
+++ src/ch5/Examples.hs
@@ -9,3 +9,27 @@ flip' f = g
 
 flip'' :: (a -> b -> c) -> b -> a -> c
 flip'' f x y = f y x
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' f (x:xs)
+    | f x == True = x : filter' f xs
+    | otherwise = filter' f xs
+
+largestDivisible :: Integer
+largestDivisible = head (filter' p [10000,9999..])
+    where p x = x `mod` 3829 == 0
+
+chain :: Integer -> [Integer]
+chain 1 = [1]
+chain n
+    | even n = n : chain (n `div` 2)
+    | odd n = n : chain (n * 3 +1)
+
+numLongChains :: Int
+numLongChains = length (filter' isLong (map' chain [1..100]))
+    where isLong xs = length xs > 15
